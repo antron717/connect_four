eaf33090e9938040afdf8f1db0f83257677e46be
Anthony <antmarklong@gmail.com>
2021-06-15 20:56:58 +0100

    checkEmpty function

--- script.js
+++ script.js
@@ -1,18 +1,27 @@
 const squares = document.querySelectorAll('td')
 
 function checkEmpty(pos) {
-    if (!pos - 7) {
-        for (let i = pos + 35; i > pos; i -= 7) {
-            if (!squares[i].classList.contains('full')) {
-                return i
-            }
+    for (let i = (pos % 7) + 35; i => pos; i -= 7) {
+        if (!squares[i].classList.contains('full')) {
+            return i
+        } else {
+            continue
         }
+        return false;
+
     }
 }
+
 for (let i = 0; i < squares.length; i++) {
     squares[i].onclick = () => {
-        for (let j = 0; j)
-            squares[i].classList.add('circle-red');
-        squares[i].innerHTML = i
+        console.log(i)
+        let pos = checkEmpty(i);
+        if (!pos) {
+            alert('Invalid Place');
+        } else {
+            squares[pos].classList.add('circle-red');
+            squares[pos].classList.add('full');
+            squares[pos].innerHTML = pos
+        }
     }
 }
