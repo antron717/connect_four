831b66d47ebd9d5a05a480dc6d142f664f64abec
Anthony <antron717@gmail.com>
2021-06-19 19:45:01 +0100

    Finished WinChecks

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const squares = document.querySelectorAll('td')
 let player = 1;
-let board = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
 
+// if square does not contain 'red or 'yellow' return postion else rerturn false
 function checkEmpty(pos) {
     let n = (pos % 7) + 35
     while (n >= pos % 7) {
@@ -15,18 +15,20 @@ function checkEmpty(pos) {
     return false;
 }
 
+// return colour at given postion
 function returnColor(pos) {
     return squares[pos].className
 }
 
+// return true if all 4 colours match and are not empty string
 function matchColorCheck(one, two, three, four) {
     return (one === two && one === three && one === four && one !== '')
 }
 
 function winCheckH() {
-    for (let row = 0; row < 36; row += 7) {
-        for (let i = 0; i < 4; i++) {
-            if (matchColorCheck(returnColor(row + i), returnColor(row + i + 1), returnColor(row + i + 2), returnColor(row + i + 3))) {
+    for (let col = 0; col < 36; col += 7) {
+        for (let row = 0; row < 4; row++) {
+            if (matchColorCheck(returnColor(col + row), returnColor(col + row + 1), returnColor(col + row + 2), returnColor(col + row + 3))) {
                 return true
             } else {
                 continue
@@ -36,6 +38,45 @@ function winCheckH() {
     return false
 }
 
+function winCheckV() {
+    for (let row = 0; row < 20; row++) {
+        if (matchColorCheck(returnColor(row), returnColor(row + 7), returnColor(row + 14), returnColor(row + 21))) {
+            return true
+        } else {
+            continue
+        }
+    }
+    return false
+}
+
+function winCheckD() {
+    for (let row = 21; row < 39; row++) {
+        if (matchColorCheck(returnColor(row), returnColor(row - 6), returnColor(row - 12), returnColor(row - 18))) {
+            return true
+        } else if (row == 24) {
+            row += 4
+            continue
+        } else {
+            continue
+        }
+        for (let row = 24; row < 42; row++) {
+            if (matchColorCheck(returnColor(row), returnColor(row - 8), returnColor(row - 16), returnColor(row - 24))) {
+                return true
+            } else if (row == 27) {
+                row += 4
+                continue
+            } else {
+                continue
+            }
+        }
+    }
+    return false
+}
+
+function winCheck() {
+    return (winCheckH() || winCheckV() || winCheckD())
+}
+
 document.querySelector('h1').innerText = 'Player: ' + player
 for (let i = 0; i < squares.length; i++) {
     squares[i].onclick = () => {
@@ -43,20 +84,26 @@ for (let i = 0; i < squares.length; i++) {
         if (pos === false) {
             alert('Invalid Place');
         } else {
-            console.log(returnColor(pos))
             if (player == 1) {
                 squares[pos].classList.add('red');
-                board[pos] = player
-                squares[pos].innerHTML = pos
+                // board[pos] = player
+                // squares[pos].innerHTML = pos
+                if (winCheck()) {
+                    document.querySelector('h1').innerText = `Player ${player} WINS!`
+                    alert(`Player ${player} wins`)
+                }
                 player = 2;
             } else {
                 squares[pos].classList.add('yellow');
-                board[pos] = player
-                squares[pos].innerHTML = pos
+                // board[pos] = player
+                // squares[pos].innerHTML = pos
+                if (winCheck()) {
+                    document.querySelector('h1').innerText = `Player ${player} WINS!`
+                    alert(`Player ${player} wins`)
+                }
                 player = 1;
             }
             document.querySelector('h1').innerText = 'Player: ' + player
-            console.log(winCheckH())
         }
     }
 }
